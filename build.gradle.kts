53ca534e9fe475a8ee4b4f9b1f21b68ecffd9635
Ahmed Maruf <t2pksa@gmail.com>
2025-02-10 21:48:14 +0300

    Revert build.gradle.kts to point to original author and original repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,14 +53,14 @@ subprojects {
          * own github profile link
          * */
         author(
-            name = "defour",
-            socialLink = "https://github.com/iiD4x",
-            image = "https://github.com/iiD4x.png"
+            name = "abrogani",
+            socialLink = "https://github.com/abrogani",
+            image = "https://github.com/abrogani.png"
         )
         // author( ... )
         // author( ... )
 
-        setRepository("https://github.com/iiD4x/automatic-sniffle")
+        setRepository("https://github.com/abrogani/automatic-sniffle")
 
         id = "abrogani-$projectName"
     }
